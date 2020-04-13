66f879584643186d23177ddb1e6badeb68826feb
Maksym Shnurenok <maksym.shnurenok@aspose.com>
2020-04-13 16:53:46 +0300

    SU-2639 - Added controllers.

---
+++ service/src/services/TemplateService/TemplateProduct.TemplateService/Controllers/TemplateServiceController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateCompany.TemplateProduct.TemplateService.Controllers
+{
+    [ApiController]
+    public class TemplateServiceController : ControllerBase
+    {
+        [HttpGet]
+        [Route("[controller]")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetTemplateService()
+        {
+            return Ok("Hello from TemplateService!");
+        }
+    }
+}
